61c7a14ca8c1d3ffe687efcaac1ba5d0730274c0
Ayanbanerjee93 <ayanbanerjee.1893@gmail.com>
2023-12-29 22:12:17 +0530

    setup done for local storage

--- src/Componnents/Cart/Cart.jsx
+++ src/Componnents/Cart/Cart.jsx
@@ -8,12 +8,19 @@ const Cart = ({cart}) => {                //option 3
 
     let total =0;
     let shipping=0;
+    let quantity=0;
 
     for(const product of cart){
 
-         total = total + product.price;
+        // if(product.quantity === 0){
+        //     product.quantity =1;
+        // }
+
+         total = total + product.price * product.quantity;
 
          shipping= shipping + product.shipping;
+
+         quantity= quantity + product.quantity;
     }
 
     const tax = total*7/100;
@@ -23,7 +30,7 @@ const Cart = ({cart}) => {                //option 3
     return (
         <div className='cart'>            
             <h4>Order summery</h4>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total price:${total}</p>
             <p>Total Shipping:${shipping}</p>
             <p>Tax:${tax.toFixed(2)}</p>
